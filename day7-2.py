dac696941fd960a7e40a17947722603ec1185a20
Olivier Lafleur <olivier.lafleur@elapsetech.com>
2020-12-08 13:25:53 -0500

    Jour 7 partie 2

---
+++ day7-2.py
@@ -0,0 +1,47 @@
+f = open("day7-puzzle-input.txt", "r")
+
+lignes = f.readlines()
+regles = dict()
+couleurs = []
+
+
+def generer_regle(ligne):
+    split = ligne.split(" bags contain")
+    couleur = split[0]
+    couleurs.append(couleur)
+    split2 = split[1].replace(" bags.", "").replace(" bag.", "").replace("no other", "").replace("\n", "")[1:]
+    split3 = split2.split(',')
+    if split3[0] == '':
+        return {couleur: []}
+
+    nombre = int(split3[0][0])
+    valeur = split3[0][2:].replace(" bags", "").replace(" bag", "")
+
+    if len(split3) == 1:
+        return {couleur: [(nombre, valeur)]}
+
+    valeurs = [(nombre, valeur)]
+    for i in range(1, len(split3)):
+        valeurs.append((int(split3[i][1]), split3[i][3:].replace(" bags", "").replace(" bag", "")))
+
+    return {couleur: valeurs}
+
+
+for ligne in lignes:
+    regle = generer_regle(ligne)
+    regles = {**regle, **regles}
+
+
+def compter(couleur):
+    if len(regles[couleur]) == 0:
+        return 0
+
+    count = 0
+
+    for paire in regles[couleur]:
+        count += paire[0] + (paire[0] * compter(paire[1]))
+
+    return count
+
+
+print(compter("shiny gold"))
