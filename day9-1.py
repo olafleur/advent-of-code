97e1e46e16fb48eb51c12fe5810424d9b4927127
Olivier Lafleur <olivier.lafleur@elapsetech.com>
2020-12-09 09:43:46 -0500

    Jour 9 partie 1

---
+++ day9-1.py
@@ -0,0 +1,23 @@
+f = open("day9-puzzle-input.txt", "r")
+
+lignes = f.readlines()
+
+entiers = list(map(lambda x: int(x), lignes))
+
+
+def somme_est_dans(entier, liste):
+    somme = False
+
+    for i in range(len(liste)-1):
+        for j in range(i, len(liste)):
+            if liste[i] + liste[j] == entier:
+                somme = True
+
+    return somme
+
+
+for i in range(25, len(entiers)):
+    sous_liste = entiers[i-25:i]
+
+    if not somme_est_dans(entiers[i], sous_liste):
+        print(entiers[i])
