33c7b4fa7d1299762775a3e69bef7876bb22f806
Olivier Lafleur <olivier.lafleur@elapsetech.com>
2020-12-09 10:27:59 -0500

    Jour 9 partie 2

---
+++ day9-2.py
@@ -0,0 +1,30 @@
+f = open("day9-puzzle-input.txt", "r")
+
+lignes = f.readlines()
+
+entiers = list(map(lambda x: int(x), lignes))
+
+nombre = 556543474
+
+for i in range(len(entiers)):
+    somme = entiers[i]
+    j = 1
+    min = entiers[i]
+    max = entiers[i]
+
+    if entiers[i] < nombre:
+        while somme < nombre:
+            somme += entiers[i + j]
+
+            if entiers[i + j] > max:
+                max = entiers[i + j]
+
+            if entiers[i + j] < min:
+                min = entiers[i + j]
+
+            j += 1
+
+        if somme == nombre:
+            print("min = " + str(min))
+            print("max = " + str(min))
+            print("résultat = " + str(min + max))
