09050114ee0f11dd2f60a696f0fd98bf71fc0b9e
Olivier Lafleur <olivier.lafleur@elapsetech.com>
2020-12-10 10:34:12 -0500

    Jour 10 partie 2

--- day10-2.py
+++ day10-2.py
@@ -10,33 +10,22 @@ joltages.append(0)
 joltages.append(max + 3)
 joltages.sort()
 
-count = 0
+i = 0
+liste = [joltages[0]]
+count = 1
+chemins = {3: 2, 4: 4, 5: 7}
 
+while i < len(joltages) - 1:
+    if joltages[i+1] - joltages[i] == 1:
+        if joltages[i] not in liste:
+            liste.append(joltages[i])
+        liste.append(joltages[i+1])
+    else:
+        nb = len(liste)
+        if nb >= 3:
+            count *= chemins[nb]
+            print(liste)
+        liste = []
+    i += 1
 
-def agrandir(liste):
-    global count
-    derniere = liste[-1]
-
-    if derniere == max + 3:
-        count += 1
-        print(liste)
-        return
-
-    if derniere + 1 in joltages:
-        liste1 = liste[:]
-        liste1.append(derniere + 1)
-        agrandir(liste1)
-    if derniere + 2 in joltages:
-        liste2 = liste[:]
-        liste2.append(derniere + 2)
-        agrandir(liste2)
-    if derniere + 3 in joltages:
-        liste3 = liste[:]
-        liste3.append(derniere + 3)
-        agrandir(liste3)
-
-    return
-
-
-agrandir([0])
 print(count)
