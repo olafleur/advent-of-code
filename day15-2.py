52b242ee270587d1637ef1b0d6beb336b9cfa3c4
Olivier Lafleur <olivier.lafleur@elapsetech.com>
2020-12-15 11:26:06 -0500

    Jour 15 partie 2

---
+++ day15-2.py
@@ -0,0 +1,26 @@
+tableau = [5, 2, 8, 16, 18, 0, 1]
+nombre_interessant = 30000000
+nombres_vus = dict()
+
+for i in range(len(tableau)):
+    nombres_vus[tableau[i]] = i
+
+
+def first_time(nb):
+    return nb not in nombres_vus
+
+
+def trouver_avant(nb):
+    return nombres_vus[nb]
+
+
+for i in range(len(tableau) - 1, nombre_interessant):
+    if first_time(tableau[i]):
+        tableau.append(0)
+    else:
+        index = trouver_avant(tableau[i])
+        tableau.append(i - index)
+
+    nombres_vus[tableau[i]] = i
+
+print(tableau[nombre_interessant - 1])
