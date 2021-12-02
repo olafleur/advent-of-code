0da9c52e2f204cee206d406b9302b3395a2eee7a
Olivier Lafleur <olivier.lafleur@gmail.com>
2021-12-02 00:14:17 -0500

    2021 Jour 2 terminé

--- 2021/day02-2.go
+++ 2021/day02-2.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	contenu, _ := os.ReadFile("day02-puzzle-input.txt")
+
+	tableau := strings.Split(string(contenu), "\n")
+	horizontal := 0
+	descente := 0
+	aim := 0
+
+	for i := range tableau {
+		commande := strings.Split(tableau[i], " ")
+		valeur, _ := strconv.Atoi(commande[1])
+		switch commande[0] {
+		case "up":
+			aim -= valeur
+		case "down":
+			aim += valeur
+		case "forward":
+			horizontal += valeur
+			descente += aim * valeur
+		}
+	}
+
+	fmt.Println(horizontal * descente)
+}
