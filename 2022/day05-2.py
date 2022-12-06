315923fe223f344da62b2c032d3429e2b3c23b24
Olivier Lafleur <olivier.lafleur@gmail.com>
2022-12-06 00:13:51 -0500

    2022 day 5 part 2

---
+++ 2022/day05-2.py
@@ -0,0 +1,34 @@
+f = open("day05-puzzle-input.txt", "r")
+
+# stacks = [
+#     ['Z', 'N'],
+#     ['M', 'C', 'D'],
+#     ['P']
+# ]
+
+stacks = [
+    ['Q', 'M', 'G', 'C', 'L'],
+    ['R', 'D', 'L', 'C', 'T', 'F', 'H', 'G'],
+    ['V', 'J', 'F', 'N', 'M', 'T', 'W', 'R'],
+    ['J', 'F', 'D', 'V', 'Q', 'P'],
+    ['N', 'F', 'M', 'S', 'L', 'B', 'T'],
+    ['R', 'N', 'V', 'H', 'C', 'D', 'P'],
+    ['H', 'C', 'T'],
+    ['G', 'S', 'J', 'V', 'Z', 'N', 'H', 'P'],
+    ['Z', 'F', 'H', 'G']
+]
+
+for ligne in f:
+    first_split = ligne[5:].strip().replace(' ', '').split('from')
+    qte = int(first_split[0])
+    second_split = first_split[1].split('to')
+    depart = int(second_split[0]) - 1
+    arrivee = int(second_split[1]) - 1
+
+    for i in range(qte):
+        stacks[arrivee].append(stacks[depart][i-qte])
+    for i in range(qte):
+        stacks[depart].pop()
+
+for ligne in stacks:
+    print(ligne[-1])
