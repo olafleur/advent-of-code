e767d38e10f6b7f5fce686d9b8d025fbb0f79521
Olivier Lafleur <olivier.lafleur@gmail.com>
2022-12-05 23:17:36 -0500

    2022 day 4 part 1

---
+++ 2022/day04-1.py
@@ -0,0 +1,13 @@
+f = open("day04-puzzle-input.txt", "r")
+
+total = 0
+
+for ligne in f:
+    splitted = ligne.split(',')
+    gauche = list(map(lambda x: int(x), splitted[0].split('-')))
+    droite = list(map(lambda x: int(x), splitted[1].strip().split('-')))
+
+    if (gauche[0] >= droite[0] and gauche[1] <= droite[1]) or (droite[0] >= gauche[0] and droite[1] <= gauche[1]):
+        total += 1
+
+print(total)
